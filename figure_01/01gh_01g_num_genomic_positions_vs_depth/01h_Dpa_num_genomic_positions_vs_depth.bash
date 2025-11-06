83a73686c2a3e374d2b6d6d97c18e056d67f273f
George Chung <69369525+gchchung@users.noreply.github.com>
2025-11-05 21:05:22 -0500

    Delete figure_01/01gh_01g_num_genomic_positions_vs_depth directory

--- figure_01/01gh_01g_num_genomic_positions_vs_depth/01h_Dpa_num_genomic_positions_vs_depth.bash
+++
@@ -1,9 +0,0 @@
-# use the samtools depth output from Figure 1e
-# and extract how many genomic positions have read coverage of n
-# using samtools_dpeth_parser.py, an interactive script
-# 
-# Enter the file name for the samtools depth output when prompted.
-# ie Dpa.aln.sorted.samtools_depth.txt
-
-
-python3 samtools_depth_parser.py
