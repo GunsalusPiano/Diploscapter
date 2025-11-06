49e49f16a531f827bb23840105cb99509a409871
George Chung <69369525+gchchung@users.noreply.github.com>
2025-11-05 19:39:00 -0500

    Fix variable name and formatting in bash script

--- figure_01/01c_Dco_read_depth/01e_Dco_genomic_read_depth.bash
+++ figure_01/01c_Dco_read_depth/01e_Dco_genomic_read_depth.bash
@@ -14,14 +14,15 @@ ALIGNMENTS="Dco.aln"
 
 minimap2 -ax map-hifi ${GENOME} ${READS} > ${ALIGNMENTS}.sam
 samtools sort -@ 16 -o ${ALIGNMENTS}.sorted.bam ${ALIGNMENTS}.sam
-samtools index -@ 16 ${ALIGNMENT}.sorted.bam ${ALIGNMENT}.sorted.bam.bai
+samtools index -@ 16 ${ALIGNMENT}.sorted.bam ${ALIGNMENTS}.sorted.bam.bai
 
 # Use samtools depth to get the depth
-samtools depth -aa -o ${ALIGNMENT}.sorted.samtools_depth.txt -@ 14 ${ALIGNMENT}.sorted.bam
+samtools depth -aa -o ${ALIGNMENTS}.sorted.samtools_depth.txt -@ 14 ${ALIGNMENTS}.sorted.bam
 
 # This depth file is very big as it contains a value for every genomic position
 # Take the arithmetic mean of depths every 100,000 bps using Python script samtools_depth_output_simplifier_v4.py
 python3 samtools_depth_output_simplifier_v4.py -d ${ALIGNMENT}.sorted.samtools_depth.txt -w 100000
 
 # The output should be ${ALIGNMENT}.sorted.samtools_depth.window100000.txt
+
 # Use R to graph the read coverage
