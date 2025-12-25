f0fdd3389c8b541ffa3d41b23dad669815e66b1e
George Chung <69369525+gchchung@users.noreply.github.com>
2025-12-25 17:34:25 -0500

    Update Dpa_get_subtelomeric_read_depths.bash

--- figure_02/02ab_subtelomeric_read_depths/Dpa_get_subtelomeric_read_depths.bash
+++ figure_02/02ab_subtelomeric_read_depths/Dpa_get_subtelomeric_read_depths.bash
@@ -7,9 +7,10 @@
 #
 # the position values are 1-based, inclusive on both ends.
 
-# from aligning Dpa ONT R9.4 genomic reads to the assembly (data from Fig. 1F, = Dpa.aln.sorted.samtools_depth.txt))
+# from aligning Dpa ONT R9.4 genomic reads to the assembly (data from Fig. 1F, = Dpa.aln.sorted.samtools_depth.txt)
 # use samtools_depth_output_simplifier_v4.py to simplify read depth values, and only focus on the terminal 80 kb
 # specified in the Dco_chromosome_ranges.txt
 python3 samtools_depth_output_simplifier_v4.py -d Dpa.aln.sorted.samtools_depth.txt -w 10 -c Dpa_chromosome_ranges.txt
+
 
 # proceed to plot the read depths using R ggplot
