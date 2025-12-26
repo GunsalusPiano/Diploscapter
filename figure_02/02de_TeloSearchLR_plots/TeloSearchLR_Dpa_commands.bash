2af83d8738485c284de79318ca65bb51ea5a4761
George Chung <69369525+gchchung@users.noreply.github.com>
2025-12-25 22:29:09 -0500

    Update TeloSearchLR_Dpa_commands.bash

--- figure_02/02de_TeloSearchLR_plots/TeloSearchLR_Dpa_commands.bash
+++ figure_02/02de_TeloSearchLR_plots/TeloSearchLR_Dpa_commands.bash
@@ -1,12 +1,17 @@
 # Use TeloSearchLR to find the telomeric repeat motif in the Porechopped Dpachys ONT reads (Dpachys_porechopped.fasta)
 TeloSearchLR.py -f Dpachys_porechopped.fasta -k 4 -K 20 -t 1000 -n 1000 -m 1 -M 100 -c 10
 
-# We find a mirrored occupancy for the pattern TAAGGGTAAGGC, the 38th most frequently found
-# pattern in the terminal 1000 bp. 
-# Upon closer inspection, there appears to be some alternation between the TAAGGG and TAAGGC motifs.
-# Fetch occupancy data for TAAGGG as well using the TideHunter table generated in the last step
-# (TeloSearchLR.k4.K20.Dpachys_porechopped.TideHunterTable.txt)
+# We find a mirrored occupancy for the pattern TAAGGGTAAGGC and its reverse complement, as the 38th most frequently found 
+# pattern in the terminal 1000 bp of the reads. Upon closer inspection, this pattern appears to be the result of alternating 
+# between the TAAGGG and TAAGGC repeat motifs.
+
+# Fetch the occupancy data of TAAGGG and TAAGGGTAAGGC, using the TideHunter table generated in the last step
+TeloSearchLR.py -s TAAGGGTAAGGC -T TeloSearchLR.k4.K20.Dpachys_porechopped.TideHunterTable.txt -f Dpachys_porechopped.fasta -n 1000
 TeloSearchLR.py -s TAAGGG -T TeloSearchLR.k4.K20.Dpachys_porechopped.TideHunterTable.txt -f Dpachys_porechopped.fasta -n 1000
 
+# Do the same with Dcoronatus HiFi reads (nxDipCoro1_hifi.fasta)
+TeloSearchLR.py -f nxDipCoro1_hifi.fasta -k 4 -K 20 -t 1000 -n 1000 -m 1 -M 100 -c 10
+TeloSearchLR.py -s TAAGGGTAAGGC -T TeloSearchLR.k4.K20.nxDipCoro1_hifi.TideHunterTable.txt -f nxDipCoro1_hifi.fasta -n 1000
+TeloSearchLR.py -s TAAGGG -T TeloSearchLR.k4.K20.nxDipCoro1_hifi.TideHunterTable.txt -f nxDipCoro1_hifi.fasta -n 1000
 
-# Proceed with graphing the occupancies using R
+# The occupancy data are ready to be graphed using R ggplot2.
