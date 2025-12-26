89ea7e2d138452d35e274cebf0a5f89582e32c15
George Chung <69369525+gchchung@users.noreply.github.com>
2025-12-25 22:12:51 -0500

    Update TeloSearchLR_Dpa_commands.bash

--- figure_02/02de_TeloSearchLR_plots/TeloSearchLR_Dpa_commands.bash
+++ figure_02/02de_TeloSearchLR_plots/TeloSearchLR_Dpa_commands.bash
@@ -1,4 +1,4 @@
-# Use TeloSearchLR to find the telomeric repeat motif in the Porechopped Dpachys ONT reads
+# Use TeloSearchLR to find the telomeric repeat motif in the Porechopped Dpachys ONT reads (Dpachys_porechopped.fasta)
 TeloSearchLR.py -f Dpachys_porechopped.fasta -k 4 -K 20 -t 1000 -n 1000 -m 1 -M 100 -c 10
 
 # We find a mirrored occupancy for the pattern TAAGGGTAAGGC, the 38th most frequently found
@@ -7,5 +7,6 @@ TeloSearchLR.py -f Dpachys_porechopped.fasta -k 4 -K 20 -t 1000 -n 1000 -m 1 -M 
 # Fetch occupancy data for TAAGGG as well using the TideHunter table generated in the last step
 # (TeloSearchLR.k4.K20.Dpachys_porechopped.TideHunterTable.txt)
 TeloSearchLR.py -s TAAGGG -T TeloSearchLR.k4.K20.Dpachys_porechopped.TideHunterTable.txt -f Dpachys_porechopped.fasta -n 1000
+
 
 # Proceed with graphing the occupancies using R
